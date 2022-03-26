3a4fc6570c73a94b62a52df41a1109fe4c8cc822
FanRende <zxcj04@gmail.com>
2022-03-26 16:22:29 +0800

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by FanRende on 2022/3/26.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
